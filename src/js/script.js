c29abe48c823088e5ef878a5a856b221a7e93489
Marysia Pabich <marysia.pabich@gmail.com>
2019-10-28 14:54:57 +0100

    Add login pop-up

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,14 @@ hamburger.addEventListener('click', function () {
 
 
 const logout = document.querySelector('.icon-quit');
+const login = document.querySelector('.icon-profile');
 const modal = document.querySelector('.pop-up');
 
 const modalQuit = document.querySelector('.pop-up__quit');
+const modalLogin = document.querySelector('.pop-up__login');
+
 const quitButton = document.querySelector('.quit');
+
 const closeButton = document.querySelector('.icon-close');
 
 
@@ -19,19 +23,28 @@ logout.addEventListener('click', function () {
   modalQuit.classList.add('active');
 });
 
+login.addEventListener('click', function () {
+  event.preventDefault();
+  modal.classList.add('active');
+  modalLogin.classList.add('active');
+});
+
 quitButton.addEventListener('click', function () {
   modal.classList.remove('active');
   modalQuit.classList.remove('active');
+  modalLogin.classList.remove('active'); 
 });
 
 closeButton.addEventListener('click', function () {
   modal.classList.remove('active');
   modalQuit.classList.remove('active');
+  modalLogin.classList.remove('active'); 
 });
 
 window.addEventListener('click', function (event) {
   if (event.target == modal) {
     modal.classList.remove('active');
-    modalQuit.classList.remove('active');    
+    modalQuit.classList.remove('active');
+    modalLogin.classList.remove('active'); 
   }
 });
